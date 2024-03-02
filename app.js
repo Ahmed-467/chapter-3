a0556173249b238a27fe2b5566e022e778217d5f
Ahmed-467 <hplaptop03421@gmail.com>
2024-03-02 15:28:48 +0500

    chapter 3

---
+++ app.js
@@ -0,0 +1,20 @@
+//qno1
+
+// var username =('i am 17 year ago');
+// alert(username);
+
+//qno2
+
+//Ans
+
+// var visited = 40;
+// alert( "You have visited this site " + visited + " times " );
+
+
+//qno3
+// Ans//
+var birthYear = 2005 ;
+alert("My birth year " + birthYear + "\nDate type of my ddeclared variable in number" );
+document.write("<h2>My birth year " + birthYear + " Date type of my ddeclared variable in number</h2>");
+
+
